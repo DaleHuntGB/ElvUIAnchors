e5c260ee38896e1fa68608fd7ea89e7d33b3142c
Dale Hunt <dalehunt@gmail.com>
2026-04-17 18:24:21 +0100

    Hook into ToggleMovers so that if necessary, frames are repositioned

--- Core.lua
+++ Core.lua
@@ -7,21 +7,14 @@ end
 
 function AddOn:OnEnable()
     Private:SetupConfig()
+    if C_AddOns.IsAddOnLoaded("ElvUI_Anchors") then Private:PrettyPrint("|cFFFFCC00ElvUI Anchors|r is |cFF40FF40installed|r. Please |cFFFF4040disable|r or |cFFFF4040uninstall|r either of the two addons.") return end
+
     local EventFrame = CreateFrame("Frame")
     EventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
     EventFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
-    EventFrame:SetScript("OnEvent", function(_, event, ...)
-        if event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_SPECIALIZATION_CHANGED" then
-            Private:SetMovers(Private.DB.global.Player.Enabled, "ElvUF_PlayerMover", table.concat(Private.DB.global.Player.Layout, ","))
-            Private:SetMovers(Private.DB.global.PlayerCastBar.Enabled, "ElvUF_PlayerCastbarMover", table.concat(Private.DB.global.PlayerCastBar.Layout, ","))
-            Private:SetMovers(Private.DB.global.Target.Enabled, "ElvUF_TargetMover", table.concat(Private.DB.global.Target.Layout, ","))
-            Private:SetMovers(Private.DB.global.TargetCastBar.Enabled, "ElvUF_TargetCastbarMover", table.concat(Private.DB.global.TargetCastBar.Layout, ","))
-            Private:SetMovers(Private.DB.global.TargetTarget.Enabled, "ElvUF_TargetTargetMover", table.concat(Private.DB.global.TargetTarget.Layout, ","))
-            Private:SetMovers(Private.DB.global.Focus.Enabled, "ElvUF_FocusMover", table.concat(Private.DB.global.Focus.Layout, ","))
-            Private:SetMovers(Private.DB.global.FocusCastBar.Enabled, "ElvUF_FocusCastbarMover", table.concat(Private.DB.global.FocusCastBar.Layout, ","))
-            Private:SetMovers(Private.DB.global.FocusTarget.Enabled, "ElvUF_FocusTargetMover", table.concat(Private.DB.global.FocusTarget.Layout, ","))
-            Private:SetMovers(Private.DB.global.Pet.Enabled, "ElvUF_PetMover", table.concat(Private.DB.global.Pet.Layout, ","))
-            Private.E:LoadMovers()
-        end
-    end)
+
+    EventFrame:SetScript("OnEvent", function(_, event, ...) if event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_SPECIALIZATION_CHANGED" then Private:SetAllMovers() end end)
+
+    hooksecurefunc(Private.E, "ToggleMovers", function() Private:SetAllMovers() end)
+
 end
