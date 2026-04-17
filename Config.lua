0e0eaa1f29fa1d5f7bf56a98c4dc17dfce7eb516
Dale Hunt <dalehunt@gmail.com>
2026-04-17 16:23:18 +0100

    Fix Frame Names

--- Config.lua
+++ Config.lua
@@ -1,18 +1,28 @@
 local Private = select(2, ...)
 local ACH = Private.ACH
+local ACR = Private.E.Libs.AceConfigRegistry
 
 local function BuildFrameLayoutSection(FrameDB, MoverName, GroupName, TabOrder)
     local Section = ACH:Group(GroupName, nil, TabOrder, "tab")
 
-    Section.args.Enabled = ACH:Toggle("Enable |cFF8080FF" .. GroupName .. "|r Anchor", nil, 1, nil, nil, "full", function() return FrameDB.Enabled end, function(_, value) FrameDB.Enabled = value Private:UpdateMovers(FrameDB.Enabled, MoverName, table.concat(FrameDB.Layout, ",")) end)
+    local function IsDisabled()
+        return not FrameDB.Enabled
+    end
+
+    local function UpdateLayout()
+        Private:UpdateMovers(FrameDB.Enabled, MoverName, table.concat(FrameDB.Layout, ","))
+        if ACR then ACR:NotifyChange("ElvUI") end
+    end
+
+    Section.args.Enabled = ACH:Toggle("Enable |cFF8080FF" .. GroupName .. "|r Anchor", nil, 1, nil, nil, "full", function() return FrameDB.Enabled end, function(_, value) FrameDB.Enabled = value UpdateLayout() end)
 
     Section.args.LayoutHeader = ACH:Header("Anchoring", 2)
 
-    Section.args.AnchorFrom = ACH:Select("Anchor From", nil, 3, Private.ANCHOR_POINTS, nil, "full", function() return FrameDB.Layout[1] end, function(_, value) FrameDB.Layout[1] = value Private:UpdateMovers(FrameDB.Enabled, MoverName, table.concat(FrameDB.Layout, ",")) end)
-    Section.args.AnchorParent = ACH:Input("Anchor Parent", nil, 4, nil, "full", function() return FrameDB.Layout[2] end, function(_, value) FrameDB.Layout[2] = value Private:UpdateMovers(FrameDB.Enabled, MoverName, table.concat(FrameDB.Layout, ",")) end)
-    Section.args.AnchorTo = ACH:Select("Anchor To", nil, 5, Private.ANCHOR_POINTS, nil, "full", function() return FrameDB.Layout[3] end, function(_, value) FrameDB.Layout[3] = value Private:UpdateMovers(FrameDB.Enabled, MoverName, table.concat(FrameDB.Layout, ",")) end)
-    Section.args.XOffset = ACH:Range("X Offset", nil, 6, { min = -2000, max = 2000, step = 1 }, "full", function() return FrameDB.Layout[4] end, function(_, value) FrameDB.Layout[4] = value Private:UpdateMovers(FrameDB.Enabled, MoverName, table.concat(FrameDB.Layout, ",")) end)
-    Section.args.YOffset = ACH:Range("Y Offset", nil, 7, { min = -2000, max = 2000, step = 1 }, "full", function() return FrameDB.Layout[5] end, function(_, value) FrameDB.Layout[5] = value Private:UpdateMovers(FrameDB.Enabled, MoverName, table.concat(FrameDB.Layout, ",")) end)
+    Section.args.AnchorFrom = ACH:Select("Anchor From", nil, 3, Private.ANCHOR_POINTS, nil, "full", function() return FrameDB.Layout[1] end, function(_, value) FrameDB.Layout[1] = value UpdateLayout() end, IsDisabled)
+    Section.args.AnchorParent = ACH:Input("Anchor Parent", nil, 4, nil, "full", function() return FrameDB.Layout[2] end, function(_, value) FrameDB.Layout[2] = value UpdateLayout() end, IsDisabled)
+    Section.args.AnchorTo = ACH:Select("Anchor To", nil, 5, Private.ANCHOR_POINTS, nil, "full", function() return FrameDB.Layout[3] end, function(_, value) FrameDB.Layout[3] = value UpdateLayout() end, IsDisabled)
+    Section.args.XOffset = ACH:Range("X Offset", nil, 6, { min = -2000, max = 2000, step = 1 }, "full", function() return FrameDB.Layout[4] end, function(_, value) FrameDB.Layout[4] = value UpdateLayout() end, IsDisabled)
+    Section.args.YOffset = ACH:Range("Y Offset", nil, 7, { min = -2000, max = 2000, step = 1 }, "full", function() return FrameDB.Layout[5] end, function(_, value) FrameDB.Layout[5] = value UpdateLayout() end, IsDisabled)
 
     return Section
 end
@@ -21,12 +31,12 @@ function Private:SetupConfig()
 	Private.Config = ACH:Group(format("%s", Private.ADDON_NAME), nil, 20, "tab")
 
     Private.Config.args.Player = BuildFrameLayoutSection(Private.DB.global.Player, "ElvUF_PlayerMover", "Player Frame", 1)
-    Private.Config.args.PlayerCastBar = BuildFrameLayoutSection(Private.DB.global.PlayerCastBar, "ElvUF_PlayerCastBarMover", "Player Cast Bar", 2)
+    Private.Config.args.PlayerCastBar = BuildFrameLayoutSection(Private.DB.global.PlayerCastBar, "ElvUF_PlayerCastbarMover", "Player Cast Bar", 2)
     Private.Config.args.Target = BuildFrameLayoutSection(Private.DB.global.Target, "ElvUF_TargetMover", "Target Frame", 3)
-    Private.Config.args.TargetCastBar = BuildFrameLayoutSection(Private.DB.global.TargetCastBar, "ElvUF_TargetCastBarMover", "Target Cast Bar", 4)
+    Private.Config.args.TargetCastBar = BuildFrameLayoutSection(Private.DB.global.TargetCastBar, "ElvUF_TargetCastbarMover", "Target Cast Bar", 4)
     Private.Config.args.TargetTarget = BuildFrameLayoutSection(Private.DB.global.TargetTarget, "ElvUF_TargetTargetMover", "Target of Target Frame", 5)
     Private.Config.args.Focus = BuildFrameLayoutSection(Private.DB.global.Focus, "ElvUF_FocusMover", "Focus Frame", 6)
-    Private.Config.args.FocusCastBar = BuildFrameLayoutSection(Private.DB.global.FocusCastBar, "ElvUF_FocusCastBarMover", "Focus Cast Bar", 7)
+    Private.Config.args.FocusCastBar = BuildFrameLayoutSection(Private.DB.global.FocusCastBar, "ElvUF_FocusCastbarMover", "Focus Cast Bar", 7)
     Private.Config.args.FocusTarget = BuildFrameLayoutSection(Private.DB.global.FocusTarget, "ElvUF_FocusTargetMover", "Focus Target", 8)
     Private.Config.args.Pet = BuildFrameLayoutSection(Private.DB.global.Pet, "ElvUF_PetMover", "Pet Frame", 9)
 
